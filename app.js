789d2b6b01bbc59568fa30486f248c6cad4e5bb1
Deedar Ahmed <deedarahmed532@gmail.com>
2023-11-22 07:52:36 +0500

    0o

---
+++ app.js
@@ -0,0 +1,66 @@
+// Math expresiion  
+
+var a = 10;
+
+console.log(a ," the value of A is 10");
+
+// ++ operator : increament the value 
+
+var a = a++;
+var a = ++a;
+
+console.log(a, " now value of A is 11");
+
+// -- operator decreament the value 
+
+var a = a--;
+
+console.log(a, " now value of A is again 10");
+
+
+var a = 2
+var b = 1
+var result = --a - --b + ++b + b-- ;
+
+document.write(" the value of a is "+a+"<br> ")
+document.write(" the value of b is "+b+"<br>")
+document.write(" the question is --a - --b + ++b + b-- <br>")
+document.write(" the answer is "+ result+" <br>")
+document.write("<h2>the end of Q.2</h2> <br><br>")
+
+
+
+
+ 
+var yourName = prompt ("Right your name")
+alert ("welcome "+ yourName+"")
+ 
+
+
+document.write("<h2>the start of Q.5</h2> <br>")
+var num = prompt("write a number for table ")
+document.write(""+num+" * 1 = "+num * 1 +" <br>")
+document.write(""+num+" * 2 = "+num * 2 +" <br>")
+document.write(""+num+" * 3 = "+num * 3 +" <br>")
+document.write(""+num+" * 4 = "+num * 4 +" <br>")
+document.write(""+num+" * 5 = "+num * 5 +" <br>")
+document.write(""+num+" * 6 = "+num * 6 +" <br>")
+document.write(""+num+" * 7 = "+num * 7 +" <br>")
+document.write(""+num+" * 8 = "+num * 8 +" <br>")
+document.write(""+num+" * 9 = "+num * 9 +" <br>")
+document.write(""+num+" * 10 = "+num * 10 +"")
+ 
+
+
+ 
+var first = prompt("write your first subject")
+var second = prompt("write your second subject")
+var third = prompt("write your third subject")
+var firstObtMark  =prompt("write first period obt marks")
+var secondObtMark  =prompt("write second period obt marks")
+var thirdObtMark  =prompt("write third period obt marks")
+document.write("<h2>"+ first+"      100    "+firstObtMark +"  <br></H2>")
+document.write(" <H2>"+ second+"      100    "+secondObtMark +"  <br> </h2>")
+document.write("<h2>"+ third+"      100    "+thirdObtMark +"  <br></h2> <br> ")
+ 
+
